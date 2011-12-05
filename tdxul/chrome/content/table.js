c682da845b6446df20ae40aacbe187cb833afa8a
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-05 16:25:23 +0100

    table.xul: creation de l'item pour la listbox

--- tdxul/chrome/content/table.js
+++ tdxul/chrome/content/table.js
@@ -7,7 +7,15 @@ function ajouterLigne() {
     var montant = document.getElementById('tbMontant').value;
     
     // créer les elements à integrer dans la listbox
-
+    var labDesc = document.createElement('label');
+    var labMontant = document.createElement('label');
+    
+    labDesc.setAttribute('value', desc );
+    labMontant.setAttribute('value', montant );
+    
+    var item = document.createElement('listitem');
+    item.appendChild(labDesc);
+    item.appendChild(labMontant);
     
     
     // insérer les nouveaux elements dans la listbox
