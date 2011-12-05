633b86a93f914dc14dab1890b726e6d9573abb00
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-05 16:17:23 +0100

    table.xul: ajout listener sur le bouton, et ajout id sur les textboxs

---
+++ tdxul/chrome/content/table.js
@@ -0,0 +1,10 @@
+
+
+
+function ajouterLigne() {
+    // récuperer les valeurs
+    
+    // créer les elements à integrer dans la listbox
+    
+    // insérer les nouveaux elements dans la listbox
+}
