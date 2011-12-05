565aed1a49d3946a53d620bb37adc3246fad10d5
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-05 16:24:58 +0100

    table.xul: listener, récupération des valeurs des textboxs

--- tdxul/chrome/content/table.js
+++ tdxul/chrome/content/table.js
@@ -3,8 +3,12 @@
 
 function ajouterLigne() {
     // récuperer les valeurs
+    var desc = document.getElementById('tbDescription').value;
+    var montant = document.getElementById('tbMontant').value;
     
     // créer les elements à integrer dans la listbox
+
+    
     
     // insérer les nouveaux elements dans la listbox
 }
