39e371e7262abe294bc36cb72c12e05e686e8bd9
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-12 15:26:20 +0100

    xbl: methodes deleteRow et modifyRow

--- comete@mozilla.org/chrome/content/table.js
+++ comete@mozilla.org/chrome/content/table.js
@@ -16,44 +16,7 @@ function rempliFormulaire() {
     document.getElementById('tbMontant').value = montant;
 }
 
-function modifierLigne() {
-    // récupération de la listbox
-    var listbox = document.getElementById('comptes');
-    // récupération de l'élement sélectionné
-    var item    = listbox.selectedItem;
-    
-    if (item == null) {
-        window.alert('Select an item');
-        return;
-    }
 
-    // récuperer les valeurs
-    var desc = document.getElementById('tbDescription').value;
-    var montant = document.getElementById('tbMontant').value;
-    
-    item.children[0].setAttribute('value', desc);
-    item.children[1].setAttribute('value', montant);
-}
-
-function supprimerLigne() {
-    // récupération de la listbox
-    var listbox = document.getElementById('comptes');
-    // récupération de l'élement sélectionné
-    var item    = listbox.selectedItem;
-    
-    if (item == null) {
-        window.alert('Select an item');
-        return;
-    }
-    
-    if (window.confirm("Are you sure to delete?")) {
-        listbox.removeChild(item);
-        // nettoyer le formulaire
-        document.getElementById('tbDescription').value = '';
-        document.getElementById('tbMontant').value = '';
-    }
-
-}
 
 
 
