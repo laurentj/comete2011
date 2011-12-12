1189d3e00c744ae5b4c21fa3d7983c9bd113c307
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-12 15:17:14 +0100

    xbl: methode addRow

--- comete@mozilla.org/chrome/content/table.js
+++ comete@mozilla.org/chrome/content/table.js
@@ -1,32 +1,6 @@
 
 
 
-function ajouterLigne() {
-    // récuperer les valeurs
-    var desc = document.getElementById('tbDescription').value;
-    var montant = document.getElementById('tbMontant').value;
-    
-    // créer les elements à integrer dans la listbox
-    var labDesc = document.createElement('label');
-    var labMontant = document.createElement('label');
-    
-    labDesc.setAttribute('value', desc );
-    labMontant.setAttribute('value', montant );
-    
-    var item = document.createElement('listitem');
-    item.appendChild(labDesc);
-    item.appendChild(labMontant);
-
-    // insérer les nouveaux elements dans la listbox
-    var listbox = document.getElementById('comptes');
-    listbox.appendChild(item);
-    
-    // nettoyer le formulaire
-    document.getElementById('tbDescription').value = '';
-    document.getElementById('tbMontant').value = '';
-}
-
-
 function rempliFormulaire() {
     // récupération de la listbox
     var listbox = document.getElementById('comptes');
