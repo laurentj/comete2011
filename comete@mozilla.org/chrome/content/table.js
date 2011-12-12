402eee8ff024ab6c9589b8381cbb1e072a56efad
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-12 10:34:55 +0100

    Ajout de la possibilité de modifier un item selectionné Deux étapes : listener sur l'evenement select pour remplir le formulaire et bouton pour modifier l'item selectionné avec les valeurs saisies

--- comete@mozilla.org/chrome/content/table.js
+++ comete@mozilla.org/chrome/content/table.js
@@ -25,3 +25,52 @@ function ajouterLigne() {
     document.getElementById('tbDescription').value = '';
     document.getElementById('tbMontant').value = '';
 }
+
+
+function rempliFormulaire() {
+    // récupération de la listbox
+    var listbox = document.getElementById('comptes');
+    // récupération de l'élement sélectionné
+    var item    = listbox.selectedItem;
+    
+    //récupération des valeurs dans chaque colonne
+    var desc    = item.children[0].getAttribute('value');
+    var montant = item.children[1].getAttribute('value');
+
+    // remplissage des textboxs
+    document.getElementById('tbDescription').value = desc;
+    document.getElementById('tbMontant').value = montant;
+}
+
+function modifierLigne() {
+    // récupération de la listbox
+    var listbox = document.getElementById('comptes');
+    // récupération de l'élement sélectionné
+    var item    = listbox.selectedItem;
+    
+    if (item == null) {
+        window.alert('Select an item');
+        return;
+    }
+
+    // récuperer les valeurs
+    var desc = document.getElementById('tbDescription').value;
+    var montant = document.getElementById('tbMontant').value;
+    
+    item.children[0].setAttribute('value', desc);
+    item.children[1].setAttribute('value', montant);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
