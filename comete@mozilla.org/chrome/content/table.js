53dcaa28c34964f591b32c6912065c9b10b2860b
Laurent Jouanneau <ljouanneau@gmail.com>
2011-12-12 11:08:36 +0100

    ajout bouton suppression

--- comete@mozilla.org/chrome/content/table.js
+++ comete@mozilla.org/chrome/content/table.js
@@ -61,7 +61,25 @@ function modifierLigne() {
     item.children[1].setAttribute('value', montant);
 }
 
+function supprimerLigne() {
+    // récupération de la listbox
+    var listbox = document.getElementById('comptes');
+    // récupération de l'élement sélectionné
+    var item    = listbox.selectedItem;
+    
+    if (item == null) {
+        window.alert('Select an item');
+        return;
+    }
+    
+    if (window.confirm("Are you sure to delete?")) {
+        listbox.removeChild(item);
+        // nettoyer le formulaire
+        document.getElementById('tbDescription').value = '';
+        document.getElementById('tbMontant').value = '';
+    }
 
+}
 
 
 
